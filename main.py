3285575cfafc9aefa3d789067cd0dd13c1f1a550
karvalian <obaid8197@gmail.com>
2022-06-16 17:35:19 +0500

    Added Speech

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import requests
 import json
+from gtts import gTTS
+from pygame import mixer
+import time
 #response = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=10").text
 response = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/summary").text
 #print(response.json())
@@ -10,4 +13,13 @@ print("Page Title: ", dict_obj.get('titles'))
 print("Page ID : ", dict_obj.get('pageid'))
 print("Description : ", dict_obj.get('description'))
 print("-----------Main Content---------- \n", dict_obj.get('extract'), " ", dict_obj.get('extract_html'))
+text = f"Display Title: {dict_obj.get('titles')} Page Title: {dict_obj.get('pageid')}, Description : {dict_obj.get('description')}, Main Content : {dict_obj.get('extract')} {dict_obj.get('extract_html')}"
+gTTSobject = gTTS(text)
+gTTSobject.save('random.mp3')
 
+mixer.init()
+mixer.music.load('random.mp3')
+mixer.music.play()
+
+while mixer.music.get_busy():
+  time.sleep(1)
